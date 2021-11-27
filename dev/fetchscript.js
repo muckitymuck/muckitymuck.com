7c12f3c0973295dd5d7500992ad6ccab4629f8f2
andre <andrewgoldstein@hotmail.com>
2021-11-27 08:56:45 -0600

    cleanup

--- dev/fetchscript.js
+++
@@ -1,8 +0,0 @@
-fetch('https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount')
-.then(function (response) {
-    return response.json();
-})
-.then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-
-})
