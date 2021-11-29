04a41bf3ade4ef0a8096e0024b9fa2a451c8e908
andre <andrewgoldstein@hotmail.com>
2021-11-29 05:48:49 -0600

    fix article files

--- use-dev-to-articles.ts
+++ use-dev-to-articles.ts
@@ -1,36 +1,27 @@
-import DevToArticle from "interfaces/dev-to-article";
-import { useState, useEffect } from "react";
+import DevToArticleMeta from "interfaces/dev-to-article-meta";
+import { useEffect, useState } from "react";
 import { DevToService } from "utils/dev-to-service";
 
 /**
-* Get a specific article given the article's slug.
-* @param slug the slug of the article to retrieve.
+* Fetch all of my published dev.to articles
 * @param onError a callback which is invoked if the request fails
 */
-export default function useDevToArticle(slug: string, onError?: () => void) {
+export default function useDevToArticles(onError?: () => void) {
   const [loading, setLoading] = useState(true);
-  const [article, setArticle] = useState<DevToArticle>();
+  const [articles, setArticles] = useState<Array<DevToArticleMeta>>([]);
 
   useEffect(() => {
-    // this bit may not be necessary for you; I needed
-    // it because I'm using Next.js server side rendering
-    // so slug is `undefined` on the initial render
-    // since I'm getting it from a route parameter
-    // e.g. /blog/:slug
-    if (slug == null || slug.length === 0) {
-      return;
-    }
-
     (async () => {
       try {
-        setArticle(await DevToService.getArticle(slug));
+        setArticles(await DevToService.fetchArticles());
       } catch (e) {
         onError?.();
       }
 
       setLoading(false);
     })();
-  }, [onError, slug]);
-  // return the article, and the loading indicator
-  return { article, loading };
+  }, [onError]);
+
+  // return the array of articles, and the loading indicator
+  return { articles, loading };
 }
